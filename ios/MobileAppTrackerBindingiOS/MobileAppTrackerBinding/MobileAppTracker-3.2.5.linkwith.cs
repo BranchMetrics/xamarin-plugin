add950ba6250f91468bbbe12dbf45414a6be057e
Harshal Ogale <harshal.ogale@hasoffers.com>
2014-09-30 12:53:26 -0700

    Removed old unused MAT iOS SDK lib files.

--- ios/MobileAppTrackerBindingiOS/MobileAppTrackerBinding/MobileAppTracker-3.2.5.linkwith.cs
+++
@@ -1,4 +0,0 @@
-using System;
-using MonoTouch.ObjCRuntime;
-
-[assembly: LinkWith ("MobileAppTracker-3.2.5.a", LinkTarget.Simulator | LinkTarget.ArmV7 | LinkTarget.ArmV7s | LinkTarget.Arm64, ForceLoad = true, Frameworks="CoreTelephony iAd MobileCoreServices SystemConfiguration")]
