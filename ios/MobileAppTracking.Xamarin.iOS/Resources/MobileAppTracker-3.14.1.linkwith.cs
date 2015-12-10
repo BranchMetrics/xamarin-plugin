7b8cf64ea97b9d033832dbcaa28c63717efea0c6
John Gu <johng@tune.com>
2015-12-09 16:47:29 -0800

    Updated for iOS SDK 3.15.0

--- ios/MobileAppTracking.Xamarin.iOS/Resources/MobileAppTracker-3.14.1.linkwith.cs
+++
@@ -1,4 +0,0 @@
-using System;
-using ObjCRuntime;
-
-[assembly: LinkWith ("MobileAppTracker-3.14.1.a", LinkTarget.Simulator, SmartLink = true, ForceLoad = true, Frameworks="AdSupport, CoreTelephony, iAd, MobileCoreServices, StoreKit, SystemConfiguration")]
---
+++ ios/MobileAppTracking.Xamarin.iOS/Resources/MobileAppTracker-3.15.0.linkwith.cs
@@ -0,0 +1,3 @@
+using ObjCRuntime;
+
+[assembly: LinkWith ("MobileAppTracker-3.15.0.a", SmartLink = true, ForceLoad = true, Frameworks="AdSupport, CoreTelephony, iAd, MobileCoreServices, StoreKit, SystemConfiguration")]
